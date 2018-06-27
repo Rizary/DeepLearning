ec54c81aeb117a51ffe12f59ac81ffa7c46b1b81
rizary <andika.riyan@gmail.com>
2018-06-28 01:07:26 +0700

    Upstream update

---
+++ datasci.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import ./nixdeps/nixpkgs.nix }:
+let
+   pkgs = import nixpkgs {};
+   env = (pkgs.python3.withPackages
+     (pythonPackages: with pythonPackages; [
+       ipykernel
+       pandas
+       scikitlearn
+       nltk
+       tensorflow
+       numpy
+       scipy
+       matplotlib
+       tqdm
+       jupyter
+       gensim
+       ]));
+  
+in pkgs.stdenv.mkDerivation rec {
+     name = "qoeifs-app";
+     src = ./.;
+     installPhase = ''
+       mkdir -p $out/src
+       cp -rf $src/* $out/src
+     '';
+     phases = ["unpackPhase" "installPhase" ];
+     buildInputs = [ env ];
+   }
+
